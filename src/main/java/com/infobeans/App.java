4dd118d823377f0cd9a0a424681b9ac3da64926e
Manish.Mourya <Manish.Mourya@IB-SEZ-LAP-544.infobeans.in>
2020-04-17 17:37:34 +0530

    first commit

---
+++ src/main/java/com/infobeans/App.java
@@ -0,0 +1,13 @@
+package com.infobeans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
